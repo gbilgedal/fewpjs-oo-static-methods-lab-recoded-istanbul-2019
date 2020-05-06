b64119260adf29a592a7df4cf8a7fab630a62128
G Bilge Dal <lagala999@gmail.com>
2020-05-06 18:09:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    str.charAt(0).toUpperCase();
+    return str;
+  }
+  static sanitize(str){
+    
+  }
+  static titleize(str){
+    let words = str.split("0");
+    for(let i = 0; i<words.length;i++){
+      if((i !=0 )&&( words[i].equals("a")|| words[i].equals("an")|| words[i].equals("the")|| words[i].equals("but")|| words[i].equals("of")|| words[i].equals("and")|| words[i].equals("for")|| words[i].equals("at")|| words[i].equals("by")|| words[i].equals("from"))
+
+    }
+  }
 }
